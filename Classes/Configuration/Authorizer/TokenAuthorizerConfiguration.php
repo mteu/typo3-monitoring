b4b3591fc88016eb67e6ded862498c83d0db6585
Martin Adler <martin.adler@init.de>
2025-07-18 12:04:55 +0200

    [BUGFIX] Fix type inconsistencies

--- Classes/Configuration/Authorizer/TokenAuthorizerConfiguration.php
+++ Classes/Configuration/Authorizer/TokenAuthorizerConfiguration.php
@@ -13,10 +13,10 @@ namespace mteu\Monitoring\Configuration\Authorizer;
 final readonly class TokenAuthorizerConfiguration implements AuthorizerConfiguration
 {
     public function __construct(
-        private ?bool $enabled = false,
-        private ?int $priority = 10,
-        public ?string $secret = '',
-        public ?string $authHeaderName = '',
+        private bool $enabled = false,
+        private int $priority = 10,
+        public string $secret = '',
+        public string $authHeaderName = '',
     ) {}
 
     public function isEnabled(): bool
